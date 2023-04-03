d3beb9cd51031ab70f4bfb20244769ec054be844
linxin4cs <119416575+linxin4cs@users.noreply.github.com>
2023-04-03 18:17:10 +0800

    feat:  删除预留 prompt 以及第二个机器人

--- config.ts
+++ config.ts
@@ -4,11 +4,6 @@ export default {
     'shanyue-gpt': {
       appId: process.env.SHANYUE_GPT_APP_ID,
       appSecret: process.env.SHANYUE_GPT_APP_SECRET,
-      prompt: '将我输入的任何语言翻译成中文，如果我输入的是中文则翻译成英文。 '
-    },
-    'shanyue-sql': {
-      appId: process.env.SHANYUE_SQL_APP_ID,
-      appSecret: process.env.SHANYUE_SQL_APP_SECRET,
       prompt: ''
     },
   },
